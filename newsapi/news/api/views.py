34c4eeb62d3fea36c93e82b46fa812e99af27555
luca92c <luca92c@gmail.com>
2020-01-15 22:52:25 +0100

    function based view to class based view

--- newsapi/news/api/views.py
+++ newsapi/news/api/views.py
@@ -4,9 +4,54 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from news.models import Article
 from news.api.serializers import ArticleSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
 
-@api_view(["GET", "POST"])
+class ArticleListCreateAPIView(APIView):
+    """Mostra un elenco degli articoli presenti
+    nel database e ne crea di nuovi"""
+
+    def get(self, request):
+        articles = Article.objects.filter(active=True)
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetailAPIView(APIView):
+    """retrieve, update, delete per un'istanza di Article"""
+
+    def get_object(self, pk):
+        article = get_object_or_404(Article, pk=pk)
+        return article
+
+    def get(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        article = self.get_object(pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+'''@api_view(["GET", "POST"])
 def article_list_create_api_view(request):
     if request.method == "GET":
         articles = Article.objects.filter(active=True)
@@ -46,3 +91,4 @@ def article_detail_api_view(request, pk):
     elif request.method == "DELETE":
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+'''
